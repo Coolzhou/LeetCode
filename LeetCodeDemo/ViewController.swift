6dc503d5c6953791e7c5eb7d3865c5df1beea719
leezhou <xeaje0000@126.com>
2018-03-21 17:51:55 +0800

    重置

--- LeetCodeDemo/ViewController.swift
+++ LeetCodeDemo/ViewController.swift
@@ -10,54 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var tabeView: UITableView!
-    
-    var dataArray = Array<String>.init(arrayLiteral: "01_wo Sum","03_Longest Substring Without Repeating Characters",
-                                       "04_Median of Two Sorted Arrays","AddStrings","LongestSubstringWithoutRepeatingCharacters")
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
-        self.tabeView.register(LeetCodeCell.self, forCellReuseIdentifier: "LeetCode")
-        self.tabeView.delegate = self
-        self.tabeView.dataSource = self
-        self.tabeView.tableFooterView = UIView.init()
-        
-    }
-    
-    func runLeetCode(_ index:Int) {
-        
-        switch index {
-        case 0:
-                let twoSum = TwoSum_1.init()
-                let nums = twoSum.towSum2([2,7,11,15], 9)
-                print("nums = \(nums)")
-            
-        case 1:
-                let leetCode_3 = LeetCode_3.init()
-                let nums = leetCode_3.lengthOfLongestSubstring("pwwkew")
-                print("nums = \(nums)")
-        case 2:
-            let leetCode_4 = LeetCode_04.init()
-            let nums = leetCode_4.findMedianSortedArrays([1,6,3], [4,5,8])
-            print("nums = \(nums)")
-            
-        case 3:
-            let leetCode_4 = AddStrings.init()
-            let nums = leetCode_4.addStrings("123456", "11")
-            print("nums = \(nums)")
-            
-        case 4:
-            let leetCode_4 = LongestSubstringWithoutRepeatingCharacters.init()
-            let nums = leetCode_4.lengthOfLongestSubstring("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcde")
-            print("nums = \(nums)")
-            
-        default:
-            print("nil")
-            
-        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -68,26 +24,6 @@ class ViewController: UIViewController {
 
 
 
-extension ViewController:UITableViewDelegate,UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.dataArray.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "LeetCode") as! LeetCodeCell
-        let titleString = self.dataArray[indexPath.row]
-        cell.textLabel?.text = titleString
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        runLeetCode(indexPath.row)
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 44
-    }
-}
 
 
 
